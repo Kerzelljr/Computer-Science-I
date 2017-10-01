865bd66e4290d35136d0c4ca730f931fa99c6de6
Kerzell Ramos <8621601+Kerzelljr@users.noreply.github.com>
2017-09-30 19:39:56 -0500

    Update FutureValues.java

--- FutureValues.java
+++ FutureValues.java
@@ -11,6 +11,8 @@ import java.text.*;
 
 public class FutureValues {
 	
+	//Variable declaration
+	//Static because can be used by all methods within the class. 
 	static double compoundIntRate, annuityIntRate, presentVal, yearlyPayments;
 	static int compoundYears, annuityYears;
 
@@ -31,6 +33,7 @@ public class FutureValues {
 		
 		System.out.println("Your account value with compound interest after " + compoundYears + " years, is $"
 							+ Math.floor(getCompoundValue()) + "\n");
+							//use Math.floor here instead of moneyString because there is no need to round cents here
 		
 		//ANNUITY CALCULATOR		
 		System.out.println("Enter your yearly payment:");
@@ -46,10 +49,16 @@ public class FutureValues {
 							+ moneyString(getAnnuityValue(yearlyPayments)));
 	}
 	
+	//Method to compute the future compound value of an account
+	//Takes the present value of the account, multiplies it by one plus the compound interest rate which
+	//is then squared to the number of years that account has been active. 
 	 public static double getCompoundValue() {
 		 return presentVal * Math.pow((1 + (compoundIntRate/100)), compoundYears);
 	}
-	
+	 
+	 //Method that computes the future annuity value of an account
+	 //Takes the amount of the yearly payment made, multiplies it by 1 plus the annuity interest rate, 
+	 //subtracts 1 from it and divides it all over the annuity interest rate. 
 	public static double getAnnuityValue(double yearlyPayment) {
 		return yearlyPayment * (Math.pow(1 + (annuityIntRate/100), annuityYears)-1 )/(annuityIntRate/100);
 	}	
@@ -58,6 +67,5 @@ public class FutureValues {
 	public static String moneyString(double amount) {
 		DecimalFormat dollarsAndCents = new DecimalFormat("$#0.00");
 		return dollarsAndCents.format(amount);
-		
 	}
 }
